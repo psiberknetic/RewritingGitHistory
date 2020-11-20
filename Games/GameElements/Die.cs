581ab69a42b97b8d1f62a8f306cb79ac734c8441
Nate Adams <psiberknetic@gmail.com>
2020-11-20 13:53:28 -0600

    Create Die class

---
+++ Games/GameElements/Die.cs
@@ -0,0 +1,12 @@
+﻿using GameElements.Interfaces;
+using System;
+
+namespace GameElements
+{
+	public class Die : IDie
+	{
+		public int Sides => throw new NotImplementedException();
+
+		public int Value => throw new NotImplementedException();
+	}
+}
