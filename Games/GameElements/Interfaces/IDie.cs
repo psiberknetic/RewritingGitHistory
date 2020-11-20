3f268e7d1209477ffb630fd898dfeab856cddcd1
Nate Adams <psiberknetic@gmail.com>
2020-11-20 13:53:28 -0600

    Create Die class

---
+++ Games/GameElements/Interfaces/IDie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameElements.Interfaces
+{
+	interface IDie
+	{
+		int Sides { get; }
+		int Value { get; }
+	}
+}
