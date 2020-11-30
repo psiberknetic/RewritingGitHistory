24131f76d83459496bcf82b2b20dd8b0f8644189
Nate Adams <psiberknetic@gmail.com>
2020-11-30 15:23:16 -0600

    Implement IsLargeStraight Logic

--- Games/GameLogic/YahtzeRollLogic.cs
+++ Games/GameLogic/YahtzeRollLogic.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using GameElements;
+using GameElements.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GameLogic
 {
 	public static class YahtzeRollLogic
 	{
+		public static bool IsValidYahtzeRoll(this IEnumerable<IDie> dice)
+		{
+			if ( dice.Count() == 5 && dice.All(d => d.Sides == 6))
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public static bool IsLargeStraight(this IEnumerable<IDie> dice)
+		{
+			if (dice.IsValidYahtzeRoll())
+			{
+				return dice.GroupBy(d => d.Value).Count() == 5;
+			}
+
+			throw new ArgumentException("Not a valid Yahtze roll", nameof(dice));
+		}
 	}
 }
