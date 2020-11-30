62b93535159a074d89b3a02b5bac51831d7036e4
Nate Adams <psiberknetic@gmail.com>
2020-11-30 14:41:27 -0600

    Implement Value property

--- Games/GameElements/Die.cs
+++ Games/GameElements/Die.cs
@@ -6,14 +6,18 @@ namespace GameElements
 	public class Die : IDie
 	{
 		private readonly int _sides;
+		private int _value;
+		private static Random _roller = new Random();
 
 		public Die(int sides)
 		{
 			_sides = sides;
+
+			_value = _roller.Next(1, _sides + 1);
 		}
 
 		public int Sides => _sides;
 
-		public int Value => throw new NotImplementedException();
+		public int Value => _value;
 	}
 }
