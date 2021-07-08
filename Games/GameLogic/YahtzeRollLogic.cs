649875b37608cc44ed05996b7e7e1c51957129a9
Nate Adams <psiberknetic@gmail.com>
2021-07-08 09:44:04 -0500

    Add IsSmallStraight Logic

--- Games/GameLogic/YahtzeRollLogic.cs
+++ Games/GameLogic/YahtzeRollLogic.cs
@@ -9,6 +9,13 @@ namespace GameLogic
 {
 	public static class YahtzeRollLogic
 	{
+		static IEnumerable<IEnumerable<int>> _validSmallStraightValues = new[]
+		{
+			new[]{1,2,3,4},
+			new[]{2,3,4,5},
+			new[]{3,4,5,6}
+		};
+
 		public static bool IsValidYahtzeRoll(this IEnumerable<IDie> dice)
 		{
 			return dice.Count() == 5 && dice.All(d => d.Sides == 6);
@@ -19,6 +26,16 @@ namespace GameLogic
 			if (dice.IsValidYahtzeRoll())
 			{
 				return dice.GroupBy(d => d.Value).Count() == 5;
+			}
+
+			throw new ArgumentException("Not a valid Yahtze roll", nameof(dice));
+		}
+
+		public static bool IsSmallStraight(this IEnumerable<IDie> dice)
+		{
+			if (dice.IsValidYahtzeRoll())
+			{
+				return _validSmallStraightValues.Any(d => dice.Select(die => die.Value).OrderBy(val => val).Intersect(d).SequenceEqual(d));
 			}
 
 			throw new ArgumentException("Not a valid Yahtze roll", nameof(dice));
