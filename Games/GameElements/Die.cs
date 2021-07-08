7c92ae9eddddf14cd6011d008ca07348ed8f06e3
Nate Adams <psiberknetic@gmail.com>
2021-07-08 13:44:46 -0500

    Implement Value and Roll

--- Games/GameElements/Die.cs
+++ Games/GameElements/Die.cs
@@ -6,19 +6,25 @@ namespace GameElements
 	public class Die : IDie
 	{
 		private readonly int _sides;
+		private int _value;
+		private static Random _roller = new Random();
 
 		public Die(int sdies)
 		{
 			_sides = sdies;
+
+			Roll();
 		}
 
 		public int Sides => _sides;
 
-		public int Value => throw new NotImplementedException();
+		public int Value => _value;
 
 		public int Roll()
 		{
-			throw new NotImplementedException();
+			_value = _roller.Next(1, _sides + 1);
+
+			return Value;
 		}
 	}
 }
