413eef73dc3016cffc9495640167f977e7a92d3e
Nate Adams <psiberknetic@gmail.com>
2021-07-14 16:35:51 -0500

    Implement value

--- Games/GameElements/Die.cs
+++ Games/GameElements/Die.cs
@@ -6,6 +6,7 @@ namespace GameElements
 	public class Die : IDie
 	{
 		private readonly int _sides;
+		private int _value;
 
 		public Die(int sides)
 		{
@@ -14,7 +15,7 @@ namespace GameElements
 
 		public int Sides => _sides;
 
-		public int Value => throw new NotImplementedException();
+		public int Value => _value;
 
 		public int Roll()
 		{
